4767539f568ca650d5c83c13dc1b9b2ff89651d0
SweetPotato <renfuture21st@gmail.com>
2016-02-05 11:59:28 +0800

    initialize project

---
+++ src/main/java/org/genjms/client/App.java
@@ -0,0 +1,13 @@
+package org.genjms.client;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
